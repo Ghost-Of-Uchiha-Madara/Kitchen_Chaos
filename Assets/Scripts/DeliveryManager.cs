aa1d1f3e9dbf8c8a371759c0ebb237633a188deb
Madara Uchiha <sankar.gamedev@gmail.com>
2025-07-28 20:29:10 +0530

    Add Game Over UI and track delivered recipes
    Introduced a new GameOverUI to display when the game ends, showing the number of successfully delivered recipes. Updated DeliveryManager to track successful deliveries and expose the count, and added a method to KitchenGameManager to check for game over state. Modified GameScene to include the new UI elements.

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -18,6 +18,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake()
     {
@@ -80,9 +81,10 @@ public class DeliveryManager : MonoBehaviour
                 if(plateContentMatchesRecipe)
                 {
                     // Player Delivered the correct recipe!
+                    successfulRecipesAmount++;
 
                     waitingRecipeSOList.RemoveAt(i);
-
+                    
                     OnRecipeComplete?.Invoke(this, EventArgs.Empty);
                     OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
 
@@ -99,6 +101,11 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 
 }
